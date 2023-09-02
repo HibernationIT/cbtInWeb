158c4e15f2d968fdc5767e850c8b9e0f96ed6167
chanhee kim <hbnation.it@gmail.com>
2023-09-02 22:48:52 +0900

    ENH: 시작 페이지 구현

---
+++ static/js/main.js
@@ -0,0 +1,7 @@
+document.querySelector("#main").addEventListener("click", start)
+
+function start() {
+  const list = document.querySelector("#list")
+  list.style.opacity = "100%"
+  list.style.left = "0px"
+}
